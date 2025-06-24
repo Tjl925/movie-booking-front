1918c19f0c4dadf0ef5eb62dd1e742b1a6919443
唐佳亮 <15966333+michael-tw@user.noreply.gitee.com>
2025-06-24 05:40:45 +0000

    回退 'Pull Request !1 : 登录'

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,15 @@
 import {createRouter, createWebHistory} from "vue-router";
-import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
 import Layout from "@/views/Layout.vue";
 import UserInfo from "@/views/user/UserInfo.vue";
 import UserList from "@/views/user/UserList.vue";
 import RestPassword from "@/views/user/RestPassword.vue";
-import Login from "@/views/Login.vue";
-import adminPage from "@/views/admin/adminPage.vue";
-import superadminPage from "@/views/superadmin/superadminPage.vue";
 
 
 
 // 定义路由关系
 const routes = [
-  {path: '/Home', component: Home},  // 一级路由
+  {path: '/login', component: Login},  // 一级路由
   {
     path: '/',
     redirect: '/login',  // 重定向
@@ -24,9 +21,6 @@ const routes = [
       {path: '/user/resetPassword', component: RestPassword},
     ]
   },
-  {path: '/Login', component: Login},
-  {path: '/admin', component: adminPage},
-  {path: '/superadmin', component: superadminPage},
 ]
 
 // 创建路由器
